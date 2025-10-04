aa5cb720147d0592fbd9e5a509dcf61995e66d86
Kabir Khan <kabirkhann1118@gmail.com>
2025-10-04 23:06:13 +0530

    login function created

--- server/controllers/userContro.js
+++ server/controllers/userContro.js
@@ -51,7 +51,7 @@ const signUp = async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
-    res.cookie("token", token, { httpOnly: true });
+    
     res.json({token})
 
     res.status(201).json({
@@ -64,8 +64,40 @@ const signUp = async (req, res) => {
   }
 };
 
-const login = (req, res) => {
-  console.log("login");
+const login = async (req, res) => {
+  const { username, password, email } = req.body;
+
+  try {
+    await connectToMongo();
+    const db = client.db("codeHub");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      userId: user._id,
+    });
+  } catch (err) {
+    console.error(`Error during login: ${err.message}`);
+    res.status(500).json({ message: "Internal server error" });
+
+  }
+
 };
 
 const getAllUsers = (req, res) => {
