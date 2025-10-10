dacea2f2eb022395466e03d3e24b4d66c7ba51ac
Kabir Khan <kabirkhann1118@gmail.com>
2025-10-10 15:22:21 +0530

    setup authContext

---
+++ client/src/utils/authContext.jsx
@@ -0,0 +1,31 @@
+import React, {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ Children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setCurrentUser(userId);
+    }
+  }, []);
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
+  return <AuthContext.Provider value={value}>{Children}</AuthContext.Provider>;
+};
